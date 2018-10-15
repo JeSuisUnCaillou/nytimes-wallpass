1efd8b0ae7a3a70335577b5492efd127d702a192
SHAMSNEJAD Valentin <valentin.shamsnejad@soprabanking.com>
2018-10-15 14:49:41 +0200

    can now remove ads

--- js/content.js
+++ js/content.js
@@ -1,6 +1,11 @@
+let removeElement = (selector) => {
+  let element = document.querySelector(selector)
+  element.parentNode.removeChild(element)
+}
+
 const overWallSelector = '[data-testid="dock-gateway"]'
 const contentSelector = '#site-content'
-let overWall = document.querySelector(overWallSelector);
+let overWall = document.querySelector(overWallSelector)
 let content = document.querySelector(contentSelector)
 let container = content.parentNode
 let overGradient = container.lastChild
@@ -8,3 +13,18 @@ let overGradient = container.lastChild
 overWall.parentNode.removeChild(overWall) // remove the actual paywall
 content.style.overflow = 'auto' // make the content scrollable again
 overGradient.parentNode.removeChild(overGradient) // remove the gradient over the content (last child of container)
+
+// This part remove ads
+const topAdSelector = '#top-wrapper'
+const bottomAdSelector = '#bottom-wrapper'
+const storyAd1Selector = '#story-ad-1-wrapper'
+const storyAd2Selector = '#story-ad-2-wrapper'
+const storyAd3Selector = '#story-ad-3-wrapper'
+const storyAd4Selector = '#story-ad-4-wrapper'
+
+removeElement(topAdSelector)
+removeElement(bottomAdSelector)
+removeElement(storyAd1Selector)
+removeElement(storyAd2Selector)
+removeElement(storyAd3Selector)
+removeElement(storyAd4Selector)
