ea3df84fb133ec1c7b58b1b7f954572c36f42659
SHAMSNEJAD Valentin <valentin.shamsnejad@soprabanking.com>
2018-10-15 14:57:15 +0200

    remove all content ads

--- js/content.js
+++ js/content.js
@@ -15,17 +15,20 @@ removeElement(overWallSelector) // remove the actual paywall
 content.style.overflow = 'auto' // make the content scrollable again
 overGradient.parentNode.removeChild(overGradient) // remove the gradient over the content (last child of container)
 
-// This part removes the ads
+// This part removes the top & bottom ads
 const topAdSelector = '#top-wrapper'
 const bottomAdSelector = '#bottom-wrapper'
-const storyAd1Selector = '#story-ad-1-wrapper'
-const storyAd2Selector = '#story-ad-2-wrapper'
-const storyAd3Selector = '#story-ad-3-wrapper'
-const storyAd4Selector = '#story-ad-4-wrapper'
-
 removeElement(topAdSelector)
 removeElement(bottomAdSelector)
-removeElement(storyAd1Selector)
-removeElement(storyAd2Selector)
-removeElement(storyAd3Selector)
-removeElement(storyAd4Selector)
+
+// This part removes the content ads
+const storyAdSelector = '#story-ad-X-wrapper'
+for (let i = 1; i < 100; i++) {
+  let selector = storyAdSelector.replace('X', i)
+  let element = document.querySelector(selector)
+  if (element) {
+    element.parentNode.removeChild(element)
+  } else {
+    break;
+  }
+}
