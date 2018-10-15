7b1e7a68296fdac48e72aaf00137cf5a2da3bdbb
SHAMSNEJAD Valentin <valentin.shamsnejad@soprabanking.com>
2018-10-15 14:51:26 +0200

    add comments

--- js/content.js
+++ js/content.js
@@ -1,8 +1,10 @@
+// This removes an element by selector
 let removeElement = (selector) => {
   let element = document.querySelector(selector)
   element.parentNode.removeChild(element)
 }
 
+// This part removes the paywall
 const overWallSelector = '[data-testid="dock-gateway"]'
 const contentSelector = '#site-content'
 let content = document.querySelector(contentSelector)
@@ -13,7 +15,7 @@ removeElement(overWallSelector) // remove the actual paywall
 content.style.overflow = 'auto' // make the content scrollable again
 overGradient.parentNode.removeChild(overGradient) // remove the gradient over the content (last child of container)
 
-// This part remove ads
+// This part removes the ads
 const topAdSelector = '#top-wrapper'
 const bottomAdSelector = '#bottom-wrapper'
 const storyAd1Selector = '#story-ad-1-wrapper'
